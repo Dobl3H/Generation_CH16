2c357885a0e1c398fb49b0016e7c2683f0d69ed5
Eduardo <eduardo.hdez.hdz@gmail.com>
2022-08-02 18:13:04 -0500

    Introduccion a JavaScript

---
+++ 3-javaScript/js-02/funcion-1.js
@@ -0,0 +1,42 @@
+"use strict";
+function miSuma(a, b) {
+  let resultado = a + b;
+  console.log("la suma es: " + resultado);
+}
+
+miSuma(2, 3);
+
+function miSuma(a, b) {
+  console.log("la suma es: " + (a + b));
+}
+miSuma(4, 6);
+
+function miProducto(a, b) {
+  return a * b;
+}
+
+console.log("el producto es: " + miProducto(4, 5));
+
+let res;
+function miResta(a, b) {
+  return a - b;
+}
+res = miResta(10, 4);
+
+console.log("la resta es: " + res);
+
+
+/* Ejercicio division */
+function division(a, b) {
+    console.log("el resultado de la division es: " + (a / b));
+  }
+  division (24, 6);
+
+  /* Ejercicio nombre */
+function nombreCompleto(a, b, c) {
+        console.log("mi nombre es: " + (a + b + c));
+  }
+  nombreCompleto ("Eduardo", "Hernandez", "Hernandez")
+
+  
+
