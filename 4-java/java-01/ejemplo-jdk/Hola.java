1b1a3b6387d727e0705c68f0defe4503ee997dfb
Eduardo <eduardo.hdez.hdz@gmail.com>
2022-08-16 18:32:41 -0500

    Introduccion a Java

---
+++ 4-java/java-01/ejemplo-jdk/Hola.java
@@ -0,0 +1,14 @@
+public class Hola {
+// esto es una clase de JDK Java
+
+	//String miVariable = "Hola mundo java ";
+
+	public static void main (String [] arg) {
+	//String miVariable = "Hola mundo java saludos a: ";
+	String miNombre = "Eduardo Hernandez";
+	
+	//es la declaracion del metodo main
+		System.out.println ("Hola saludos a: " + miNombre);
+	}
+
+}
---
+++ ejercicios-codigo/Prueba1/src/main/java/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        //String miVariable = "Hola mundo Java"
+        String nombre = "Eduardo";
+        System.out.println("saludos desde Java a: " + nombre);
+    }
+}
