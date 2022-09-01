5709a41547665a6a86b902596f572f6a6d585b54
Eduardo <eduardo.hdez.hdz@gmail.com>
2022-09-01 12:06:52 -0500

    Continuacion Java y prueba con usuario y fallos

---
+++ 4-JDKjava/PooJavaAvanzado/src/com/ehh/excepcion/ErrorFisicoException.java
@@ -0,0 +1,7 @@
+package com.ehh.excepcion;
+
+public class ErrorFisicoException extends Exception{
+    public ErrorFisicoException(Exception ex) {
+        super("\t\t\t <-- Ocurrio un Error Fisico --> ",ex);
+    }
+}
--- 4-JDKjava/PooJavaAvanzado/src/com/ehh/excepcion/Usuario.java
+++ 4-JDKjava/PooJavaAvanzado/src/com/ehh/excepcion/Usuario.java
@@ -1,41 +1,40 @@
 package com.ehh.excepcion;
 
-    public class Usuario {
-        private String usrname;
-        private String password;
-        private String nombre;
-        private String email;
+public class Usuario {
+    private String usrname;
+    private String password;
+    private String nombre;
+    private String email;
 
-        public String getUsrname() {
-            return usrname;
-        }
+    public String getUsrname() {
+        return usrname;
+    }
 
-        public String getPassword() {
-            return password;
-        }
+    public String getPassword() {
+        return password;
+    }
 
-        public String getNombre() {
-            return nombre;
-        }
+    public String getNombre() {
+        return nombre;
+    }
 
-        public String getEmail() {
-            return email;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public void setUsrname(String usrname) {
-            this.usrname = usrname;
-        }
+    public void setUsrname(String usrname) {
+        this.usrname = usrname;
+    }
 
-        public void setPassword(String password) {
-            this.password = password;
-        }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-        public void setNombre(String nombre) {
-            this.nombre = nombre;
-        }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
