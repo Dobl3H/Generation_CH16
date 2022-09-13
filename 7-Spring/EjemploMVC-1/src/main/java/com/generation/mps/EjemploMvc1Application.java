dedb1586991eca60cd3f83ef670aed51eb29c431
Dobl3H <eduardo.hdez.hdz@gmail.com>
2022-09-13 10:16:20 -0500

    Introduccion a Spring

---
+++ 7-Spring/EjemploMVC-1/src/main/java/com/generation/mps/EjemploMvc1Application.java
@@ -0,0 +1,13 @@
+package com.generation.mps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploMvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploMvc1Application.class, args);
+	}
+
+}
---
+++ 7-Spring/EjemploMVC-1/src/test/java/com/generation/mps/EjemploMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.mps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploMvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
