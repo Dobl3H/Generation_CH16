988d4488a36994fdf7d02e031e36fac9f2a1821d
Dobl3H <eduardo.hdez.hdz@gmail.com>
2022-09-15 17:15:26 -0500

    Actualizacion

--- 7-Spring/SpringJpa/src/main/java/com/generation/demo/controllers/UsuarioControllers.java
+++ 7-Spring/SpringJpa/src/main/java/com/generation/demo/controllers/UsuarioControllers.java
@@ -4,20 +4,26 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.generation.demo.models.UsuarioModel;
 import com.generation.demo.services.UsuarioServices;
 
 @RestController
 @RequestMapping("/usuario")
+
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE,
+        RequestMethod.PUT })
 
 public class UsuarioControllers {
 	@Autowired
