47d2adb8a336e4270bb934d94b23f9ac6afb639a
Dobl3H <eduardo.hdez.hdz@gmail.com>
2022-09-21 18:00:03 -0500

    Continuacion Spring y test

---
+++ SpringPruebasUnitarias/src/main/java/com/generation/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.generation.repository;
+
+import org.springframework.data.jpa.repository.Repository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public class UsuarioRepository extends JpaRepository <UsuarioModel, Integer>{
+
+}
